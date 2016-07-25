2650a1b12f06a20864734412f11642148273cd7d
Alex Nava <alex.il.nava@gmail.com>
2016-07-26 01:28:05 +0200

    Stub for controls

--- js/ui-mgr.js
+++ js/ui-mgr.js
@@ -1,7 +1,17 @@
 var UiMgr = function(glMgrObject, inputMgrObject, fontMgrObject) {
+	this.controlType = {
+		AREA: 0,         // Generic control with all callbacks
+		BUTTON: 1,       // Pushbutton. TODO check if it can be used for fullscreen
+		CHECKBOX: 2,     // Pushbutton with an ON/OFF state
+		RADIOBUTTON: 3,  // tbd
+		TEXTINPUT: 4     // Textual input. TODO: use html control for mobile compatibility
+	};
+
 	this.glMgr = glMgrObject;
 	this.inputMgr = inputMgrObject;
 	this.fontMgr = fontMgrObject;
+
+	this.controls = [];
 }
 
 // controls with these methods:
@@ -20,3 +30,10 @@ var UiMgr = function(glMgrObject, inputMgrObject, fontMgrObject) {
 // make it mobile friendly so no crazy keyboard handlers for text input, try to use html5 controls (maybe hidden)
 //
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+
+UiMgr.prototype.removeControls = function() {
+	this.controls = [];
+}
+
+UiMgr.prototype.addControl = function() {
+}
