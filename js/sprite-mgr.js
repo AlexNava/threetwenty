081c3d9fc5508b2233deec520e8a39318ac0b750
Alessandro Nava <alex.il.nava@gmail.com>
2019-05-21 01:13:19 +0200

    First implementation of sprites manager

---
+++ js/sprite-mgr.js
@@ -0,0 +1,150 @@
+var SpriteMgr = function (glMgrObject) {
+	/*
+    this.alignments = {
+		LEFT: 1,
+		CENTER: 2,
+		RIGHT: 3
+	};*/
+
+	this.glMgr = glMgrObject;
+	this.sprites = [];
+    
+    // Desired features:
+    // - multiple spritesheets per sprite
+    // - handle different frame times (really?)
+    // - handle automatic transitions (e.g. back to idle) (really?)
+    // - define state machine per animations (just states are fine)
+    
+	//this.currentFont = "";
+	//this.currentScale = 1.0;
+	//this.currentRotation = 0.0;
+	//this.currentHAlignment = this.alignments.LEFT;
+	//this.currentColor = [1, 1, 1, 1];
+};
+
+SpriteMgr.prototype.loadSpriteFiles = function (spriteAlias, animAlias, jsonFile, bitmapFile) { // add framerate, repeat?
+	var tempSprite = this.sprites[spriteAlias];
+    if (tempSprite === undefined) {
+        tempSprite = {};
+        this.sprites[spriteAlias] = tempSprite;
+    }
+    
+    var tempAnim = {};
+    if (tempSprite.animations === undefined) {
+        tempSprite.animations = {};
+    }
+    tempSprite.animations[animAlias] = tempAnim;
+	
+	// load file, manage in callback
+	$.get(jsonFile,
+		function(data) {
+			var jsonContent = data;
+
+			//var xmlParse = $.parseXML(xmlContent);
+            var jsonParse = JSON.parse(jsonContent)
+            jsonParse.frames; // just add everything to tempAnim?
+        
+			//var font = xmlParse.getElementsByTagName("font")[0];
+			//var common = font.getElementsByTagName("common")[0];
+			//var chars = font.getElementsByTagName("chars")[0];
+			//var charCount = chars.getElementsByTagName("char").length;
+
+			//tempFont.scaleW = parseInt(common.getAttribute("scaleW"));
+			//tempFont.scaleH = parseInt(common.getAttribute("scaleH"));
+
+			for (var i = 0; i < charCount; i++) {
+				var xmlChar = chars.getElementsByTagName("char")[i];
+				var tempChar = {};
+
+				tempChar.id =        parseInt(xmlChar.getAttribute("id"));
+				tempChar.width =     parseInt(xmlChar.getAttribute("width"));
+				tempChar.height =    parseInt(xmlChar.getAttribute("height"));
+				tempChar.x =         parseInt(xmlChar.getAttribute("x"));
+				tempChar.y =         tempFont.scaleH - (parseInt(xmlChar.getAttribute("y")) + tempChar.height);
+				tempChar.xoffset =   parseInt(xmlChar.getAttribute("xoffset"));
+				tempChar.yoffset =   parseInt(xmlChar.getAttribute("yoffset"));
+				tempChar.xadvance =  parseInt(xmlChar.getAttribute("xadvance"));
+				
+				tempFont.charArray[tempChar.id] = tempChar;
+			}
+			
+			this.glMgr.loadTexture("Sprite-texture-" + spriteAlias + "-" + animAlias, bitmapFile);
+			
+		}.bind(this),
+		"text"
+	);
+
+	//this.glMgr.loadTexture("Font-texture-" + alias, bitmapFile);
+};
+
+FontMgr.prototype.setAlignment = function(alignment) {
+	if (alignment.toUpperCase() == "LEFT") {
+		this.currentHAlignment = this.alignments.LEFT;
+	}
+	else if (alignment.toUpperCase() == "CENTER") {
+		this.currentHAlignment = this.alignments.CENTER;
+	}
+	else if (alignment.toUpperCase() == "RIGHT") {
+		this.currentHAlignment = this.alignments.RIGHT;
+	}
+}
+
+FontMgr.prototype.setScale = function(scale) {
+	this.currentScale = scale;
+}
+
+FontMgr.prototype.setRotation = function(rotation) {
+	this.currentRotation = rotation;
+}
+
+FontMgr.prototype.setColor = function(r, g, b, a) {
+	this.currentColor = [r, g, b, a];
+}
+
+FontMgr.prototype.drawTextXy = function (text, x, y, fontAlias) {
+	var currentFont = this.fonts[fontAlias];
+	if (currentFont === undefined) {
+		return;
+	}
+
+	// Calculate length
+	var textLength = 0;
+	if (this.currentHAlignment !== this.alignments.LEFT) // Not needed for this
+	{
+		for (var i = 0; i < text.length; i++) {
+			var id = text.charCodeAt(i);
+			var currentChar = currentFont.charArray[id];
+			if (currentChar !== undefined) {
+				textLength += this.currentScale * currentChar.xadvance;
+			}
+		}
+	}
+
+	var currentX;
+	if (this.currentHAlignment === this.alignments.LEFT) {
+		currentX = x;
+	}
+	else if (this.currentHAlignment === this.alignments.CENTER) {
+		currentX = Math.floor(x - (textLength / 2));
+	}
+	else if (this.currentHAlignment === this.alignments.RIGHT) {
+		currentX = x - textLength;
+	}
+
+	// Draw text
+	this.glMgr.rect2DColor(this.currentColor[0], this.currentColor[1], this.currentColor[2], this.currentColor[3]);
+	//this.glMgr.rect2DRotation(this.currentRotation);
+	this.glMgr.useTexture("Font-texture-" + fontAlias);
+	for (var i = 0; i < text.length; i++) {
+		var id = text.charCodeAt(i);
+		var currentChar = currentFont.charArray[id];
+		if (currentChar !== undefined) {
+			this.glMgr.texturedRect2D(currentX, y,
+			                          this.currentScale * currentChar.width, this.currentScale * currentChar.height,
+			                          currentChar.x, currentChar.y,
+			                          currentChar.width, currentChar.height);
+
+			currentX += this.currentScale * currentChar.xadvance;
+		}
+	}
+}
