b232092899fd66dbca29b61b0dc3251aea91ebdb
Alessandro Nava <alex.il.nava@gmail.com>
2019-06-15 01:43:02 +0200

    added touch support, fixed indentation

--- demos/wingedd.js
+++ demos/wingedd.js
@@ -3,10 +3,10 @@ var startFunc = function () {
 	wgl.createFrameBuffer('mainFB');
 	initTextures();
 	initShaders();
-    
-    sprite.loadSpriteFiles("emi", "walk", "emi/emi_walk.json", "emi/emi_walk.png");
-    sprite.loadSpriteFiles("fio", "walk", "fio/fio_walk.json", "fio/fio_walk.png");
-    sprite.loadSpriteFiles("obstacles", "none", "obstacles/obstacles.json", "obstacles/obstacles.png");
+
+	sprite.loadSpriteFiles("emi", "walk", "emi/emi_walk.json", "emi/emi_walk.png");
+	sprite.loadSpriteFiles("fio", "walk", "fio/fio_walk.json", "fio/fio_walk.png");
+	sprite.loadSpriteFiles("obstacles", "none", "obstacles/obstacles.json", "obstacles/obstacles.png");
 };
 
 initTextures = function() {
@@ -17,7 +17,7 @@ initTextures = function() {
 //	wgl.loadTexture("mouse", "images/pointer.png");
 //	input.setPointer("mouse", 0, 0, 8, 8, 0, 7);
 
-    wgl.loadTexture("ground", "bg/ground.png");
+	wgl.loadTexture("ground", "bg/ground.png");
 
 };
 
@@ -50,19 +50,19 @@ var displayFunc = function(elapsed) {
 	font.setAlignment("CENTER");
 	if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
 		font.drawTextXy("Go fullscreen",
-		                160, 100, "nokia");
+						160, 100, "nokia");
 	}
 	else {
 		font.drawTextXy("Canvas is fullscreen",
-		                160, 100, "nokia");
+						160, 100, "nokia");
 	}
 
 	font.setAlignment("LEFT");
 	font.drawTextXy("Canvas size: " + wgl.mainCanvas.width + "x" + wgl.mainCanvas.height,
-	                0, 0, "nokia");
+					0, 0, "nokia");
 	font.drawTextXy("Document body size: " + document.body.clientWidth + "x" + document.body.clientHeight,
-	                0, 10, "nokia");
-	
+					0, 10, "nokia");
+
 	input.drawPointer();
 
 	//----------------------------------------------------------------------------------------------
@@ -105,32 +105,32 @@ var displayFunc = function(elapsed) {
 	wgl.gl.enable(wgl.gl.BLEND);
 	wgl.gl.blendFunc(wgl.gl.SRC_ALPHA, wgl.gl.ONE_MINUS_SRC_ALPHA);
 /*
-    font.setAlignment('LEFT');
-    font.setColor(0.2, 0.2, 0.2, 1);
-    font.drawTextXy('Time  ' + totalTime, 10, 220, 'nokia');
-    font.drawTextXy('Frame ' + cnt,       10, 210, 'nokia');
-    font.drawTextXy('Speed ' + speed,     10, 200, 'nokia');
-    font.drawTextXy('Dist. ' + distance,  10, 190, 'nokia');
-    font.drawTextXy('Vert. ' + vertSpeed, 10, 180, 'nokia');
-    font.drawTextXy('Alt.  ' + altitude,  10, 170, 'nokia');
+	font.setAlignment('LEFT');
+	font.setColor(0.2, 0.2, 0.2, 1);
+	font.drawTextXy('Time  ' + totalTime, 10, 220, 'nokia');
+	font.drawTextXy('Frame ' + cnt,       10, 210, 'nokia');
+	font.drawTextXy('Speed ' + speed,     10, 200, 'nokia');
+	font.drawTextXy('Dist. ' + distance,  10, 190, 'nokia');
+	font.drawTextXy('Vert. ' + vertSpeed, 10, 180, 'nokia');
+	font.drawTextXy('Alt.  ' + altitude,  10, 170, 'nokia');
 */
-    //font.drawTextXy('Cactus ' + Math.floor(x[1] + (obstacleX[0] - distance) * 40),  10, 170, 'nokia');
-    var groundOffset = Math.floor(-distance * 40) % 128;
+	//font.drawTextXy('Cactus ' + Math.floor(x[1] + (obstacleX[0] - distance) * 40),  10, 170, 'nokia');
+	var groundOffset = Math.floor(-distance * 40) % 128;
 	wgl.rect2DColor(1, 1, 1, 1);
-    wgl.useTexture("ground");
-    wgl.gl.texParameteri(wgl.gl.TEXTURE_2D, wgl.gl.TEXTURE_WRAP_S, wgl.gl.REPEAT);
-    wgl.gl.texParameteri(wgl.gl.TEXTURE_2D, wgl.gl.TEXTURE_WRAP_T, wgl.gl.REPEAT);
-    wgl.texturedRect2D(groundOffset, 0, 512, 64,
-                       0, 0, 512, 64, 0);
-    
-    sprite.setColor(1, 1, 1, 1);
-    sprite.setScale(1);
-    sprite.drawSprite(Math.floor(x[0]), 40 + Math.floor(altitude[0] * 50), "emi", "walk", cnt[0]);
-    sprite.drawSprite(Math.floor(x[1]), 40 + Math.floor(altitude[1] * 50), "fio", "walk", cnt[1]);
-    
-    sprite.drawSprite(Math.floor(x[1] + (obstacleX[0] - distance) * 40), 40, "obstacles", "none", 0);
-    
-    // draw textured quad from first FBO to screen
+	wgl.useTexture("ground");
+	wgl.gl.texParameteri(wgl.gl.TEXTURE_2D, wgl.gl.TEXTURE_WRAP_S, wgl.gl.REPEAT);
+	wgl.gl.texParameteri(wgl.gl.TEXTURE_2D, wgl.gl.TEXTURE_WRAP_T, wgl.gl.REPEAT);
+	wgl.texturedRect2D(groundOffset, 0, 512, 64,
+					   0, 0, 512, 64, 0);
+
+	sprite.setColor(1, 1, 1, 1);
+	sprite.setScale(1);
+	sprite.drawSprite(Math.floor(x[0]), 40 + Math.floor(altitude[0] * 50), "emi", "walk", cnt[0]);
+	sprite.drawSprite(Math.floor(x[1]), 40 + Math.floor(altitude[1] * 50), "fio", "walk", cnt[1]);
+
+	sprite.drawSprite(Math.floor(x[1] + (obstacleX[0] - distance) * 40), 40, "obstacles", "none", 0);
+
+	// draw textured quad from first FBO to screen
 	wgl.useFrameBuffer(null);
 	wgl.gl.viewport(0, 0, wgl.mainCanvas.width, wgl.mainCanvas.height);
 
@@ -139,134 +139,151 @@ var displayFunc = function(elapsed) {
 
 	wgl.useTextureFromFrameBuffer('mainFB');
 
-    if (wgl.shaders["CRT"] !== undefined) {
+	if (wgl.shaders["CRT"] !== undefined) {
 		wgl.fullscreenRectangle("CRT");
 	}
 	else {
 		wgl.gl.useProgram(wgl.shaders["texture"]);
 		wgl.gl.uniformMatrix4fv(wgl.shaders["texture"].uPMatrix, false, wgl.orthoProjMatrix);
 
-        wgl.fullscreenRectangle("texture");
+		wgl.fullscreenRectangle("texture");
 	}
 };
 
 var updateObstacle = function(create) {
-    if (obstacleX[0] === undefined || obstacleX[0] < (distance) - 1) { // obstacle not on screen
-        obstacleX.shift();
-        if (create === true) {
-            var nextX = distance + wgl.xResolution / 40 + Math.random() * 3;
-            obstacleX.push(nextX);
-        }
-    }
+	if (obstacleX[0] === undefined || obstacleX[0] < (distance) - 1) { // obstacle not on screen
+		obstacleX.shift();
+		if (create === true) {
+			var nextX = distance + wgl.xResolution / 40 + Math.random() * 3;
+			obstacleX.push(nextX);
+		}
+	}
 };
 
 var animateFun = function (elapsed) {
 	input.pollTouchGestures();
-    if (status == 1)
-    {
-        totalTime = 0;
-        speed = 0;
-        distance = 0;
-        lastStep = 0;
-        altitude = [0, 0];
-        vertSpeed = [0, 0];
-    }
-    else if (status == 2)
-    {
-        updateObstacle(true);
-        
-        x = [20, -50];
-        totalTime += elapsed * 0.001;
-        speed = 3;
-        distance += elapsed * 0.001 * speed;
-        walk(elapsed);
-        if (totalTime >= 10)
-            ++status; // enter 2nd character
-    }
-    else if (status == 3)
-    {
-        updateObstacle(); // no obstacles
+	if (status == 1)
+	{
+		totalTime = 0;
+		speed = 0;
+		distance = 0;
+		lastStep = 0;
+		altitude = [0, 0];
+		vertSpeed = [0, 0];
+	}
+	else if (status == 2)
+	{
+		updateObstacle();
 
-        x = [20, -50];
-        x[0] += 15 * (totalTime - 10);
-        x[1] += 30 * (totalTime - 10);
-        totalTime += elapsed * 0.001;
-        speed = 3;
-        if (totalTime > 20)
-            speed += 0.05 * (totalTime - 20);
+		x = [20, -50];
+		totalTime += elapsed * 0.001;
+		speed = 3;
+		distance += elapsed * 0.001 * speed;
+		walk(elapsed);
+		if (totalTime >= 10)
+			++status; // enter 2nd character
+	}
+	else if (status == 3)
+	{
+		updateObstacle(); // no obstacles
 
-        distance += elapsed * 0.001 * speed;
-        walk(elapsed);
+		x = [20, -50];
+		x[0] += 15 * (totalTime - 10);
+		x[1] += 30 * (totalTime - 10);
+		totalTime += elapsed * 0.001;
+		speed = 3;
+		if (totalTime > 20)
+			speed += 0.05 * (totalTime - 20);
 
-        if (totalTime >= 12)
-            ++status; // run as a couple        
-    }
-    else if (status == 4)
-    {
-        updateObstacle(true);
+		distance += elapsed * 0.001 * speed;
+		walk(elapsed);
 
-        totalTime += elapsed * 0.001;
-        speed = 3;
-        if (totalTime > 20)
-            speed += 0.05 * (totalTime - 20);
+		if (totalTime >= 12)
+			++status; // run as a couple        
+	}
+	else if (status == 4)
+	{
+		updateObstacle(true);
 
-        distance += elapsed * 0.001 * speed;
-        walk(elapsed);
-    }
-    
+		totalTime += elapsed * 0.001;
+		speed = 3;
+		if (totalTime > 20)
+			speed += 0.05 * (totalTime - 20);
+
+		distance += elapsed * 0.001 * speed;
+		walk(elapsed);
+	}
+
 
 };
 
+var leftPressed = false;
+var rightPressed = false;
+var checkPress = function() {
+	leftPressed = false;
+	rightPressed = false;
+
+	for (var i = 0; i < input.touchPoints.length; ++i) {
+		if (input.touchPoints[i] != undefined) {
+			if (input.touchPoints[i].pixelX < wgl.xResolution / 2)
+				leftPressed = true;
+			if (input.touchPoints[i].pixelX > wgl.xResolution / 2)
+				rightPressed = true;
+		}
+	}
+}
+
 var walk = function(elapsed) {
-    if ((distance - lastStep) > 0.4) // 6 frames = 2 steps = 2.4m
-    {
-        lastStep = distance;
-        if ((altitude[0] < 0.1) || (cnt[0] != 1 && cnt[0] != 4))
-            cnt[0]++;
-        if (cnt[0] >= 6)
-            cnt[0] = 0;
+	checkPress();
+	if ((distance - lastStep) > 0.4) // 6 frames = 2 steps = 2.4m
+	{
+		lastStep = distance;
+		if ((altitude[0] < 0.1) || (cnt[0] != 1 && cnt[0] != 4))
+			cnt[0]++;
+		if (cnt[0] >= 6)
+			cnt[0] = 0;
 
-        if ((altitude[1] < 0.1) || (cnt[1] != 1 && cnt[1] != 4))
-            cnt[1]++;
-        if (cnt[1] >= 6)
-            cnt[1] = 0;
-    }
+		if ((altitude[1] < 0.1) || (cnt[1] != 1 && cnt[1] != 4))
+			cnt[1]++;
+		if (cnt[1] >= 6)
+			cnt[1] = 0;
+	}
 
-    if (input.keyPressed[input.keyCodes.KEY_J] === true) { // 1st command J
-        if ((altitude[0] < 0.5) && (vertSpeed[0] == 0 || vertSpeed[0] > 2))
-        {
-            if (vertSpeed[0] <= 0)
-                cnt[0]++ // do a step when touching the ground
-            if (cnt[0] >= 6)
-                cnt[0] = 0;
-            vertSpeed[0] = 3;
-        }
-    }
-    if (input.keyPressed[input.keyCodes.KEY_F] === true) { // 2nd command F
-        if ((altitude[1] < 0.5) && (vertSpeed[1] == 0 || vertSpeed[1] > 2))
-        {
-            if (vertSpeed[1] <= 0)
-                cnt[1]++ // do a step when touching the ground
-            if (cnt[1] >= 6)
-                cnt[1] = 0;
-            vertSpeed[1] = 3;
-        }
-    }
+	if (rightPressed || input.keyPressed[input.keyCodes.KEY_J] === true) { // 1st command J
+		if ((altitude[0] < 0.5) && (vertSpeed[0] == 0 || vertSpeed[0] > 2))
+		{
+			if (vertSpeed[0] <= 0)
+				cnt[0]++ // do a step when touching the ground
+			if (cnt[0] >= 6)
+				cnt[0] = 0;
+			vertSpeed[0] = 3.5;
+		}
+	}
+	if (leftPressed || input.keyPressed[input.keyCodes.KEY_F] === true) { // 2nd command F
+		if ((altitude[1] < 0.5) && (vertSpeed[1] == 0 || vertSpeed[1] > 2))
+		{
+			if (vertSpeed[1] <= 0)
+				cnt[1]++ // do a step when touching the ground
+			if (cnt[1] >= 6)
+				cnt[1] = 0;
+			vertSpeed[1] = 3.5;
+		}
+	}
 
-    vertSpeed[0] -= 9.81 * elapsed * 0.001;
-    altitude[0] += vertSpeed[0] * elapsed * 0.001;
-    if (altitude[0] < 0)
-    {
-        altitude[0] = 0;
-        vertSpeed[0] = 0;
-    }
-    vertSpeed[1] -= 9.81 * elapsed * 0.001;
-    altitude[1] += vertSpeed[1] * elapsed * 0.001;
-    if (altitude[1] < 0)
-    {
-        altitude[1] = 0;
-        vertSpeed[1] = 0;
-    }
+	vertSpeed[0] -= 9.81 * elapsed * 0.001;
+	altitude[0] += vertSpeed[0] * elapsed * 0.001;
+	if (altitude[0] < 0)
+	{
+		altitude[0] = 0;
+		vertSpeed[0] = 0;
+	}
+	vertSpeed[1] -= 9.81 * elapsed * 0.001;
+	altitude[1] += vertSpeed[1] * elapsed * 0.001;
+	if (altitude[1] < 0)
+	{
+		altitude[1] = 0;
+		vertSpeed[1] = 0;
+	}
 }
 
 var wgl = new WebGlMgr();
